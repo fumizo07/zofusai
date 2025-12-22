cb61812c0ab8c8a1d80b3b09779e5b4bf0eed078
fumizo07 <87250591+fumizo07@users.noreply.github.com>
2025-12-22 10:02:28 +0900

    Create app_context.py

---
+++ app_context.py
@@ -0,0 +1,8 @@
+# app_context.py
+from collections import deque
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+RECENT_SEARCHES = deque(maxlen=5)
+EXTERNAL_SEARCHES = deque(maxlen=15)
