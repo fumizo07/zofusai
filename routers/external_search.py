eb787c838a121ab7b8b8c21a1bc540d83e3fc410
fumizo07 <87250591+fumizo07@users.noreply.github.com>
2026-03-28 17:03:42 +0900

    Update external_search.py

--- routers/external_search.py
+++ routers/external_search.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import json
 import re
+import unicodedata
 from collections import defaultdict
 from datetime import datetime
 from typing import List, Optional, Dict, Tuple
